1df28d9002514da8cc0b10c84bbd7c04ff91fae3
Alejandro Cruz <hcruz0716@gmail.com>
2022-11-01 00:03:36 -0600

    url imagen

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="h-screen flex w-full bg-img vx-row no-gutter">
     <div class="m-auto">
       <div class="w-full flex items-center justify-center">
-        <img src="@/images/logos/logo-white.svg" />
+        <img src="@/assets/images/logos/logo-white.svg" />
       </div>
       <div class="flex justify-center mb-3">
         <v-btn to="/login" class="px-12 mt-8">Continue to dashboard</v-btn>

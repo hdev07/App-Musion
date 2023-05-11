522cdd828d7182c96c12e33995d741277b7cebc4
Alejandro Cruz <hcruz0716@gmail.com>
2023-05-11 13:23:44 -0600

    Add infoView

--- pages/map/index.vue
+++ pages/map/index.vue
@@ -11,10 +11,12 @@
 import Navbar from "@/components/common/navbar.vue";
 import Search from "@/components/common/search.vue";
 import MuseumsMap from "@/components/map/museums/museumsMap.vue";
+import Card from "@/components/common/card.vue";
+
 export default {
   name: "Map",
   layout: "empty",
-  components: { Navbar, Search, MuseumsMap },
+  components: { Navbar, Search, MuseumsMap, Card },
   data() {
     return {
       activeTab: "map",
@@ -46,6 +48,7 @@ export default {
               lat: Number(museum?.coordinates?.lat),
               lng: Number(museum?.coordinates?.lng),
             },
+            infoText: this.setInfoWindoContentByMuseum(museum),
           };
           this.markers.push(marker);
         });
@@ -53,8 +56,37 @@ export default {
         console.error(e);
       }
     },
+    setInfoWindoContentByMuseum(museum) {
+      return `
+      <div class="w-full flex flex-wrap p-2">
+        <div class="w-full flex border-b border-primary mb-4 pb-1">
+          <div class="w-2/3 flex items-center">
+            <p class="truncate text-lg font-semibold text-center text-gray-900 mb-1">
+              ${museum.name}
+            </p>
+          </div>
+        </div>
+        <div class="w-full flex flex-wrap">
+          <p class="w-full flex flex-wrap mb-3S text-gray-900">
+            ${museum?.description}
+          </p>
+          <div class="w-full flex flex-wrap mb-1 text-gray-900">
+            ${museum?.address?.streetAddress} ${museum?.address?.addressLocality} ${museum?.address?.postalCode} ${museum?.address?.addressCountry} ${museum?.address?.city}
+          </div>
+          <div class="w-full text-end m-1">
+            <a
+              href="https://www.google.com/maps/search/?api=1&query=${museum.lat},${museum.lng}"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="underline"
+            >
+              Obtener direcciones
+            </a>
+          </div>
+        </div>
+      </div>
+      `;
+    },
   },
 };
 </script>
-
-<style></style>

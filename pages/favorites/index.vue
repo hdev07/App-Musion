6fa6e1143c7fb480a0bb63ff6ca8570b7f3af98c
Alejandro Cruz <hcruz0716@gmail.com>
2023-05-16 19:56:59 -0600

    Add museums Fav and filters

--- pages/favorites/index.vue
+++ pages/favorites/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    <Search />
-    <Card />
+    <Search @search="handleSearch" />
+    <div v-for="(card, i) in museums" :data="card" :key="i">
+      <Card
+        :title="museums[i]?.name"
+        :description="museums[i]?.description"
+        :location="returLocationString(museums[i].address)"
+      />
+    </div>
     <div class="relative h-44" />
-    <Pagination />
+    <Pagination
+      :perPage="perPage"
+      :totalPages="lastPage"
+      :currentPage="currentPage"
+      @changePage="handleChangePage"
+    />
     <Navbar :activeTab="activeTab" @update:activeTab="updateActiveTab" />
   </div>
 </template>
@@ -19,12 +30,59 @@ export default {
   components: { Navbar, Search, Card, Pagination },
   data() {
     return {
-      activeTab: "fav",
+      activeTab: "list",
+      museums: [],
+      perPage: 1,
+      currentPage: 1,
+      lastPage: 1,
     };
   },
+
+  async fetch() {
+    try {
+      const queryParams = this.paramsToString(this.$route.query);
+      const { data } = await this.$axios.get(`/favorites?${queryParams}`);
+      const { perPage, currentPage, lastPage, data: favorites } = data;
+      this.museums = data.favorites;
+      this.perPage = perPage;
+      this.currentPage = currentPage;
+      this.lastPage = lastPage;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+
+  watch: {
+    "$route.query": "$fetch",
+  },
+
   methods: {
     updateActiveTab(tab) {
       this.activeTab = tab;
+    },
+
+    handleSearch(search) {
+      const query = this.$route.query;
+      this.$router.push({ query: { ...query, page: 1, search } });
+    },
+
+    handleChangePage(page) {
+      const query = this.$route.query;
+      this.$router.push({ query: { ...query, page } });
+    },
+
+    returLocationString(address) {
+      let addrss =
+        address?.streetAddress +
+        " " +
+        address?.addressLocality +
+        " " +
+        address?.postalCode +
+        " " +
+        address?.addressCountry +
+        " " +
+        address?.city;
+      return addrss;
     },
   },
 };
